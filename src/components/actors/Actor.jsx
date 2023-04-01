0d5058f325a7ab4f51d7207d4f37aa59719e8316
Ahmed <ahmedayad328@gmail.com>
2023-04-01 23:26:48 +0300

    some shit additions

--- src/components/actors/Actor.jsx
+++ src/components/actors/Actor.jsx
@@ -1,18 +1,24 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState  } from 'react'
+import Slider from "react-slick";
 import axios from 'axios'
 import { useLocation } from 'react-router';
 import './actor.css'
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import Particles from '../partical/Partical'
 
 
 const Actor = () => {
     const BASE_API = 'https://api.themoviedb.org/3'
     const API_KEY = '632bf4fb465f1296a555eed5ecee4ced'
     const API_IMG = 'https://image.tmdb.org/t/p/original/';
+    const MOVIE_IMG = 'https://image.tmdb.org/t/p/w300/';
+
     const location = useLocation();
     const id = location.state;
 
     const [actorDetails, setActorDetails] = useState({})
-
+    const [actorMovies, setActorMovies] = useState([])
     const fetchActorDetails = async () => {
         const { data } = await axios.get(`${BASE_API}/person/${id}`, {
             params: {
@@ -24,13 +30,29 @@ const Actor = () => {
         setActorDetails(data)
     }
 
+    const fetchActorMovies =async () =>{
+        const {data} = await axios.get(`${BASE_API}/person/${id}/movie_credits`,{
+            params:{
+                api_key: API_KEY,
+                language: 'en-US'
+            }
+        })
+
+        setActorMovies(data.cast)
+    }
+
     useEffect(() => {
         fetchActorDetails();
+        fetchActorMovies();
     }, [])
+
+
+  
 
     return (
         <div>
-            <img src="https://img.freepik.com/free-vector/abstract-blue-light-pipe-speed-zoom-black-background-technology_1142-9120.jpg?w=1060&t=st=1680306337~exp=1680306937~hmac=c6754a13b20830019b4a621cdabb97d02ff1589c2fc73b9b16465b7e4bca99ec" alt="background-image" className='background'/>
+        <Particles className='particle'/>
+           
             <div className='actor-profile'>
                 <div>
 
@@ -41,8 +63,29 @@ const Actor = () => {
                 </div>
                 <p>{actorDetails.birthday}</p>
 
+                <h2>Movies</h2>
+            <div className='actor-movies'>
+
+            
+                 {actorMovies.slice(0,20).map((movie)=>{
+                      return (<div>
+                      <img src={MOVIE_IMG + movie.poster_path} alt="" />
+                      <p>{movie.original_title}</p>
+                      </div>
+                      )
+                      
+                      
+                      
+                     })}
+                    
+
+                   
+                     </div>
+
+                </div>
             </div>
-        </div>
+                
+        
     )
 }
 
